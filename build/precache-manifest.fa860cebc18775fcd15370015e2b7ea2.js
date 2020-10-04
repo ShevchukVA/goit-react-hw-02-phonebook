6584683a32f19c8a82aeb7492e7d3fd80e89d89b
ShevchukVA <60571733+ShevchukVA@users.noreply.github.com>
2020-10-04 20:58:28 +0300

    audit input

---
+++ build/precache-manifest.fa860cebc18775fcd15370015e2b7ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed824922855664e30072a9a11fe8a930",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "e32398a4af148693ee19",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.b647056c.chunk.css"
+  },
+  {
+    "revision": "b651f45abb14fec78625",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d9b3519b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d9b3519b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32398a4af148693ee19",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.f32c5ec8.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
---
+++ src/components/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './contactForm/ContactForm';
+import ContactList from './contactList/ContactList';
+import Filter from './filter/Filter';
+
+export default class PhoneBook extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleAddContact = ({ name, number }) => {
+    const contact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+
+    if (name === '' || number === '') {
+      return contact;
+    } else {
+      this.setState(prevState => {
+        return {
+          contacts: [...prevState.contacts, contact],
+        };
+      });
+    }
+  };
+
+  handleRemoveContact = idContact => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== idContact,
+        ),
+      };
+    });
+  };
+
+  handleFilterContacts = filter => {
+    this.setState({ filter });
+  };
+
+  handleShowFilterContact = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  handleBanContact = ({ name }) => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.map(contact => {
+          if (contact.name.toLowerCase() === name.toLowerCase()) {
+            alert(`${name} is already in contacts.`);
+          }
+          return contact;
+        }),
+      };
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.handleShowFilterContact();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          onAddContact={this.handleAddContact}
+          onBanContact={this.handleBanContact}
+        />
+        <h2>Contacts</h2>
+        {visibleContacts.length > 1 && (
+          <Filter value={filter} onChangeFilter={this.handleFilterContacts} />
+        )}
+        <ContactList
+          contacts={visibleContacts}
+          onRemoveContact={this.handleRemoveContact}
+        />
+      </div>
+    );
+  }
+}
